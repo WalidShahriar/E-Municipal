980638d8c7b59832351dc8f34e4d0819b73efcfd
sumayaEVA <eva22205101042@diu.edu.bd>
2025-12-09 10:59:41 +0600

    updated_route

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ServiceRequestController; 
 
 
-// --- NEW API ROUTES FOR SERVICE REQUESTS ---
+
 Route::post('/api/requests', [ServiceRequestController::class, 'store']);
 Route::get('/api/requests/{id}', [ServiceRequestController::class, 'show']);
-// ------------------------------------------
+
 
 
 Route::get('/home', function () {
